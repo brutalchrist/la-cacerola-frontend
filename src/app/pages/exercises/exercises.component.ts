26b60d5c97a94b3d35ca9c6571c40eca8ba93cdc
Sebastián González <brutalchrist@gmail.com>
2021-05-02 01:30:41 -0400

    change less to sass

--- src/app/pages/exercises/exercises.component.ts
+++ src/app/pages/exercises/exercises.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-exercises',
   templateUrl: './exercises.component.html',
-  styleUrls: ['./exercises.component.less']
+  styleUrls: ['./exercises.component.sass']
 })
 export class ExercisesComponent implements OnInit {
   public exercises = [];
