0dbff0f88437de1e715dea68bed856fbf59c9629
Sebastián González <brutalchrist@gmail.com>
2021-05-02 03:19:56 -0400

    add button

--- src/app/pages/products/products.module.ts
+++ src/app/pages/products/products.module.ts
@@ -4,12 +4,13 @@ import { CommonModule } from "@angular/common";
 import { NzDescriptionsModule } from 'ng-zorro-antd/descriptions';
 import { NzDividerModule } from 'ng-zorro-antd/divider';
 import { NzListModule } from 'ng-zorro-antd/list';
+import { NzButtonModule } from 'ng-zorro-antd/button';
 
 import { ProductsRoutingModule } from './products-routing.module';
 import { ProductsComponent } from './products.component';
 
 @NgModule({
-  imports: [CommonModule, ProductsRoutingModule, NzDescriptionsModule, NzDividerModule, NzListModule],
+  imports: [CommonModule, ProductsRoutingModule, NzDescriptionsModule, NzDividerModule, NzListModule, NzButtonModule],
   declarations: [ProductsComponent],
   exports: [ProductsComponent]
 })
