6b61471c4993e9d54b97b0e809cec551cf0b65aa
Sebastián González <brutalchrist@gmail.com>
2021-05-02 02:31:14 -0400

    add orders

--- src/app/pages/exercises/exercises-routing.module.ts
+++ src/app/pages/exercises/exercises-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ExercisesComponent } from './exercises.component';
+import { OrdersComponent } from './orders/orders.component';
 
 const routes: Routes = [
   { path: '', component: ExercisesComponent },
+  { path: ':id/orders', component: OrdersComponent },
 ];
 
 @NgModule({
---
+++ src/app/pages/exercises/orders/orders.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-orders',
+  templateUrl: './orders.component.html',
+  styleUrls: ['./orders.component.sass']
+})
+export class OrdersComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit() {
+    console.log('ofa');
+  }
+}
