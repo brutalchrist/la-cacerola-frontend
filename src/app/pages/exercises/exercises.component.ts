2774d9d30ed8709f75374c975035857fb1f5333b
Sebastián González <brutalchrist@gmail.com>
2021-05-09 22:14:21 -0400

    update year

--- src/app/pages/exercises/exercises.component.ts
+++ src/app/pages/exercises/exercises.component.ts
@@ -9,12 +9,12 @@ export class ExercisesComponent implements OnInit {
   public exercises = [
     {
       id: 1,
-      date: new Date("2019-04-24 00:00"),
+      date: new Date('2021-04-24 00:00'),
       published: false
     },
     {
       id: 2,
-      date: new Date("2019-05-08 00:00"),
+      date: new Date('2021-05-08 00:00'),
       published: true
     }
   ];
